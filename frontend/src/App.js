8b3c33b85b77a942915aa82eafad69faaec51cb5
Nelson Carrasquel <carrasquel@outlook.com>
2023-04-04 11:02:38 -0400

    bootstrap properly installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,26 @@ import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import TodosList from './components/todo-list';
+import AddTodo from './components/add-todo';
+import Login from './components/login';
+import Signup from './components/signup';
+
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App(){
   return (
     <div className='App'>
-      Hello World!
+      <Navbar bg="primary" variant="dark">
+        <div className="container-fluid">
+          <Navbar.Brand>React-bootstrap</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+          </Nav>
+        </div>
+      </Navbar>
     </div>
   )
 }
