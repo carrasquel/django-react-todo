49a83eb49ea32b9e2d714b4ced1234a8759f693f
Nelson Carrasquel <carrasquel@outlook.com>
2023-04-04 21:34:01 -0400

    first state variables

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,43 @@ import Signup from './components/signup';
 
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
 
 function App(){
+
+  const [user, setUser] = React.useState(null);
+  const [token, setToken] = React.useState(null);
+  const [error, setError] = React.useState('');
+
+  async function login(user = null){
+    setUser(user);
+  }
+
+  async function logout(){
+    setUser(null);
+  }
+
+  async function signup(user = null){
+    setUser(user);
+  }
+
   return (
     <div className='App'>
       <Navbar bg="primary" variant="dark">
         <div className="container-fluid">
-          <Navbar.Brand>React-bootstrap</Navbar.Brand>
+          <Navbar.Brand>Todos App</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Container>
+              <Link className="nav-link" to={"/todos"}></Link>
+              { user ? (
+                <Link className="nav-link">Logout ({user})</Link>
+              ) : (
+                <>
+                  <Link className="nav-link" to={"/login"}>Login</Link>
+                  <Link className="nav-link" to={"/signup"}>Signup</Link>
+                </>
+              )}
+            </Container>
           </Nav>
         </div>
       </Navbar>
