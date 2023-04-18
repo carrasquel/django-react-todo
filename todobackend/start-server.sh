a79ee30d68fdd767d0e53ad8ff64d7bce57786f3
Nelson Carrasquel <carrasquel@outlook.com>
2023-04-18 00:14:11 -0400

    docker compose file added

--- todobackend/start-server.sh
+++ todobackend/start-server.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 # start-server.sh
+
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres..."
+
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+      sleep 0.1
+    done
+
+    echo "PostgreSQL started"
+fi
+
+python manage.py flush --no-input
+python manage.py migrate
+
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --no-input)
 fi
